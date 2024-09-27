f85a3b721042c0b653fcd8e5902a4784c4715754
Marten de Bruijn <martendebruijn@outlook.com>
2024-09-27 16:47:49 +0200

    build website

---
+++ dist/assets/index-udDAEZKm.js
@@ -0,0 +1,5 @@
+(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const s of r.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&i(s)}).observe(document,{childList:!0,subtree:!0});function n(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=n(e);fetch(e.href,r)}})();const c=t=>t===0,f=t=>t===5,u=t=>t===6,d=t=>!u(t)||!c(t),l=t=>u(t)||c(t);function a(t){const o=t.getDay(),n=t.getHours(),i=d(o)&&n>18,e=f(o)&&n>=16;return i||e||l(o)?"beer":"tea"}const p=new Date,y=a(p);document.querySelector("#app").innerHTML=`
+  <div>
+    <p>Time for ${y==="beer"?"🍺":"🫖"}</p>
+  </div>
+`;
